c89a864c6fca41cfb861df83c7c3e5e9ac5d44da
coderaiser <coderaiser@cloudcmd.io>
2021-02-19 20:53:16 +0200

    feature(package) madrun v8.8.0

---
+++ .madrun.mjs
@@ -0,0 +1,15 @@
+import {run} from 'madrun';
+
+export default {
+    'watch:coverage': () => run('watcher', 'npm run coverage'),
+    'watch:test': () => run('watcher', 'npm test'),
+    'watcher': () => 'nodemon -w test -w lib --exec',
+    'test': () => 'tape test/*.js',
+    'coverage': () => 'c8 npm test',
+    'report': () => 'c8 report --reporter=text-lcov | coveralls',
+    'lint': () => 'putout .',
+    'fresh:lint': () => run('lint', '--fresh'),
+    'lint:fresh': () => run('lint', '--fresh'),
+    'fix:lint': () => run('lint', '--fix'),
+};
+
